01971424030cb98c90fc694d659de829a33512dc
ivandjoh <ivandi.djoh@gmail.com>
2024-06-11 15:13:02 +0800

    styles updated

--- src/app/constants/MenuLists.ts
+++ src/app/constants/MenuLists.ts
@@ -3,20 +3,24 @@ export const MENU_LISTS = [
         id: '1',
         menuTitle: 'Home',
         pathTitle: '/',
+        path: '/'
     },
     {
         id: '2',
-        menuTitle: 'Popular Movies',
-        pathTitle: '/popular',
+        menuTitle: 'Popular',
+        pathTitle: 'popular',
+        path: '/popular'
     },
     {
         id: '3',
-        menuTitle: 'Upcoming Movies',
-        pathTitle: '/upcoming',
+        menuTitle: 'Upcoming',
+        pathTitle: 'upcoming',
+        path: '/upcoming'
     },
     {
         id: '4',
         menuTitle: 'Favourites',
-        pathTitle: '/favourites',
+        pathTitle: 'favourites',
+        path: '/favourites'
     }
 ]
